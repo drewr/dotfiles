acc9a9f4dfe14734c0ea0a4aada07139dd5e5c3c
Drew Raines <aaraines@gmail.com>
2012-09-10 10:17:57 -0500

    XMonad & xfce.

--- d/.xmonad/xmonad.hs
+++ d/.xmonad/xmonad.hs
@@ -1,13 +1,7 @@
 import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import XMonad.Config.Xfce
 
-main = xmonad defaultConfig
-        { modMask = mod4Mask
-        , terminal = "urxvt"
- --       , manageHook = manageDocks <+> manageHook defaultConfig
- --       , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        }
+main = xmonad xfceConfig
+            { modMask = mod4Mask }
+
+
