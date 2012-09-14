88d35e4c641f18b6db60ff8207fcc819fafd715d
Drew Raines <aaraines@gmail.com>
2012-09-14 11:01:24 -0500

    Activate hack for intellij.

--- d/.xmonad/xmonad.hs
+++ d/.xmonad/xmonad.hs
@@ -1,14 +1,15 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.EwmhDesktops
+-- import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Hooks.SetWMName
 import System.IO
 
 main = xmonad defaultConfig
               { manageHook = manageDocks <+> manageHook defaultConfig
               , layoutHook = avoidStruts $ layoutHook defaultConfig
               , modMask    = mod4Mask
+              , startupHook = setWMName "LG3D"
               }
-
