0d00d309030eb845a531c7dcc4478ca3354de878
Drew Raines <aaraines@gmail.com>
2012-11-29 09:36:57 -0600

    Ignore libnotify events.

--- d/.xmonad/xmonad.hs
+++ d/.xmonad/xmonad.hs
@@ -5,10 +5,18 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import XMonad.Hooks.SetWMName
+import XMonad.Actions.CopyWindow
 import System.IO
 
+myManageHook :: ManageHook
+myManageHook = composeAll
+   [ manageDocks
+   , manageHook defaultConfig
+   , className =? "Xfce4-notifyd" --> doIgnore
+   ]
+
 main = xmonad $ defaultConfig
-              { manageHook = manageDocks <+> manageHook defaultConfig
+              { manageHook = myManageHook
               , layoutHook = avoidStruts $ layoutHook defaultConfig
               , modMask    = mod4Mask
               , startupHook = setWMName "LG3D"
