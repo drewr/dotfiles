d4e05ef1ca299f311580a11041e643ebe10efe97
Drew Raines <aaraines@gmail.com>
2013-01-28 13:58:54 -0600

    Add blacklist.

--- hs/dhcp2rootdns.hs
+++ hs/dhcp2rootdns.hs
@@ -4,13 +4,13 @@ import System.Cmd
 import System.Environment
 import Data.String.Utils (join)
 
+avoid = ["127.0.0.1", "192.168.5.10"]
+
 main :: IO ()
 main = do
   let outFile = "/etc/perp/dnscache/root/servers/@"
   args <- getArgs
-  let out = join "\n" $
-              filter (\ip -> (ip /= "127.0.0.1" &&
-                              ip /= "192.168.5.10")) args
+  let out = join "\n" $ filter (`notElem` avoid) args
   writeFile outFile (out ++ "\n")
   exit <- rawSystem "perpctl" ["-b", "/etc/perp", "t", "dnscache"]
   return ()
