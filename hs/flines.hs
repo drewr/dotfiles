463f7e02355b086bd83c0b44d687c45a4e945dc6
Drew Raines <aaraines@gmail.com>
2013-01-28 14:14:22 -0600

    Take blacklist as args and read input from stdin.

--- hs/flines.hs
+++ hs/flines.hs
@@ -2,15 +2,11 @@
 
 import System.Cmd
 import System.Environment
-import Data.String.Utils (join)
-
-avoid = ["127.0.0.1", "192.168.5.10"]
 
 main :: IO ()
 main = do
-  let outFile = "/etc/perp/dnscache/root/servers/@"
-  args <- getArgs
-  let out = join "\n" $ filter (`notElem` avoid) args
-  writeFile outFile (out ++ "\n")
-  exit <- rawSystem "perpctl" ["-b", "/etc/perp", "t", "dnscache"]
-  return ()
+  inputs <- getContents
+  avoid <- getArgs
+  putStrLn $ (unlines . filter (`notElem` avoid) . words) inputs
+
+
