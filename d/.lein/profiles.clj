9df9e31abd117f3b98ebc0c8287bb5e1f8458145
Drew Raines <aaraines@gmail.com>
2013-02-02 23:11:26 -0600

    Add lein profiles.

---
+++ d/.lein/profiles.clj
@@ -0,0 +1,12 @@
+{:user
+ {:plugins [
+            [cljs-template "0.1.5"]
+            [lein-clojars "0.9.0"]
+            [lein-difftest "2.0.0"]
+            [lein-marginalia "0.7.0"]
+            [lein-pprint "1.0.0"]
+            [lein-swank "1.4.5"]
+            [lein-localrepo "0.4.0"]
+            [lein-tar "1.0.2"]
+            [lein-pprint "1.1.1"]
+            ]}}
