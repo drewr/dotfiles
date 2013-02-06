49cb42439b71d7daf4d3cffa43765b53338bbb61
Drew Raines <aaraines@gmail.com>
2013-02-06 13:25:47 -0600

    Use runit instead of perp.

--- d/bin/update-dns.hs
+++ d/bin/update-dns.hs
@@ -8,12 +8,12 @@ avoid = [
  , "192.168.5.10"  -- bogus router at friend's office
  ]
 
-hupCache = rawSystem "perpctl" ["-b", "/etc/perp", "t", "dnscache"]
+hupCache = rawSystem "sv" ["term", "/etc/service/dnscache"]
 
 main :: IO ()
 main = do
   (iface:updown:_) <- getArgs
-  let rootFile = "/etc/perp/dnscache/root/servers/@"
+  let rootFile = "/etc/service/dnscache/root/servers/@"
   dnsIPs <- getEnv "DHCP4_DOMAIN_NAME_SERVERS"
   let out = unlines $ filter (`notElem` avoid) $ words dnsIPs
   writeFile rootFile out
