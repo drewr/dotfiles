bfd59884e8a30ec93130fa7e751246237a332c5d
Drew Raines <aaraines@gmail.com>
2013-02-20 14:37:31 -0600

    Sort!

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -4,10 +4,10 @@
             [com.palletops/pallet-lein "0.6.0-beta.4"]
             [lein-clojars "0.9.0"]
             [lein-difftest "2.0.0"]
+            [lein-localrepo "0.4.0"]
             [lein-marginalia "0.7.0"]
             [lein-pprint "1.0.0"]
-            [lein-swank "1.4.5"]
-            [lein-localrepo "0.4.0"]
-            [lein-tar "1.0.2"]
             [lein-pprint "1.1.1"]
+            [lein-swank "1.4.5"]
+            [lein-tar "1.0.2"]
             ]}}
