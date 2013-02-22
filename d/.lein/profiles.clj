2c2d01a618251590e623f57acaf6372419fc4d1d
Drew Raines <aaraines@gmail.com>
2013-02-21 23:46:38 -0600

    Why were there two of those?

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -6,7 +6,6 @@
             [lein-difftest "2.0.0"]
             [lein-localrepo "0.4.0"]
             [lein-marginalia "0.7.0"]
-            [lein-pprint "1.0.0"]
             [lein-pprint "1.1.1"]
             [lein-swank "1.4.5"]
             [lein-tar "1.0.2"]
