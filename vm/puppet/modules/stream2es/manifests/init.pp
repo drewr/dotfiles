4986183277c68dea87ee07435a73adc46eb5074e
Andrew Raines <andrew@raines.me>
2014-02-20 01:14:08 -0600

    Add svc control to stream2es

--- vm/puppet/modules/stream2es/manifests/init.pp
+++ vm/puppet/modules/stream2es/manifests/init.pp
@@ -1,40 +1,83 @@
 include wget
 
-class stream2es() {
+class stream2es(
+  $user="nobody",
+  $group="nogroup",
+  $heap="256m",
+  $broker="http://localhost:9200",
+  $exchange="queue",
+  $queue="s2e.default",
+  $target="http://localhost:9200/foo/t",
+) {
   $stream2es = "/usr/local/bin/stream2es"
+  $stream2es_svc_path = "/etc/svc/stream2es"
+  $stream2es_service = "/etc/service/stream2es"
 
   package {"daemontools": ensure => "present" }
+  package {"daemontools-run": ensure => "present" }
 
   package {"openjdk-7-jdk": ensure => "present" }
-  
+
+  service { "svscan":
+    ensure => running,
+    provider => "upstart",
+  }
+
   wget::fetch { "stream2es":
     source      => "http://download.elasticsearch.org/stream2es/stream2es",
     destination => $stream2es,
     timeout     => 0,
     verbose     => false,
   }
-               
+
   file { $stream2es:
-    mode => 755,
+    mode => 0755,
     owner   => "root",
     group   => "root",
     require => Exec["wget-stream2es"],
   }
 
   file { ["/etc/svc",
-          "/etc/svc/stream2es",
-          "/etc/svc/stream2es/log"]:
+          "${stream2es_svc_path}",
+          "${stream2es_svc_path}/log"]:
     ensure  => directory,
     owner   => "root",
     group   => "root",
     mode    => 0755,
   }
 
-  file { "/etc/svc/stream2es/log/main":
+  file { "${stream2es_svc_path}/log/main":
     ensure  => directory,
     owner   => "nobody",
     group   => "nogroup",
     mode    => 0755,
   }
 
+  file { "${stream2es_svc_path}/run":
+    owner   => "root",
+    group   => "root",
+    mode    => 0755,
+    content => template("$module_name/svc/run"),
+    require => File["${stream2es_svc_path}", "${stream2es_svc_path}/log/run"],
+  }
+
+  file { "${stream2es_svc_path}/log/run":
+    owner   => "root",
+    group   => "root",
+    mode    => 0755,
+    content => template("$module_name/svc/log/run"),
+    require => File["${stream2es_svc_path}/log"],
+  }
+
+  file { $stream2es_service:
+    ensure => "link",
+    target => "${stream2es_svc_path}",
+    require => File["${stream2es_svc_path}/run",
+                    "${stream2es_svc_path}/log/run"],
+  }
+
+  exec { "/usr/bin/svc -t ${stream2es_service}":
+    subscribe => File["${stream2es_svc_path}/run"],
+    refreshonly => true
+  }
 }
