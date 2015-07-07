5e5d0186c688c8c5bc2294d266a329f899f88c62
Drew Raines <drew@raines.me>
2015-07-07 11:54:18 -0500

    Refresh lein plugins

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -1,16 +1,12 @@
 {:user
  {:plugins [
             [cider/cider-nrepl "0.9.1"]
-            [lein-typed "0.3.5"]
-            [cljs-template "0.1.5"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]
-            [lein-clojars "0.9.0"]
-            [lein-difftest "2.0.0"]
-            [lein-localrepo "0.4.0"]
-            [lein-marginalia "0.7.0"]
-            [lein-pprint "1.1.1"]
-            [lein-swank "1.4.5"]
-            [lein-tar "1.0.2"]
+            [lein-localrepo "0.5.3"]
+            [lein-marginalia "0.8.0"]
+            [lein-pprint "1.1.2"]
+            [lein-tar "3.3.0"]
+            [lein-typed "0.3.5"]
             ]
   :dependencies [[pjstadig/humane-test-output "0.6.0"]]
   :injections [(require 'pjstadig.humane-test-output)
