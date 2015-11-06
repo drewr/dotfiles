fa392722257deb7f6931dfd713ab5dd30946a46b
Drew Raines <drew@raines.me>
2015-11-06 12:01:25 -0600

    cider-nrepl 0.10.0-SNAPSHOT

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user
  {:plugins [
-            [cider/cider-nrepl "0.10.0-20150910.083528-24"]
+            [cider/cider-nrepl "0.10.0-SNAPSHOT"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]
             [lein-localrepo "0.5.3"]
             [lein-marginalia "0.8.0"]
