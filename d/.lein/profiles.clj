15a953136eda98b74cca608f0a54eccf6316d30b
Drew Raines <drew@raines.me>
2016-04-16 14:38:01 -0500

    Add lein-ancient

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -2,6 +2,7 @@
  {:plugins [
             [cider/cider-nrepl "0.12.0-SNAPSHOT"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]
+            [lein-ancient "0.6.10"]
             [lein-localrepo "0.5.3"]
             [lein-marginalia "0.8.0"]
             [lein-pprint "1.1.2"]
