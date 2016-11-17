6cd4422705adeeac41378970be1242d2998b9b39
Drew Raines <drew@raines.me>
2016-11-17 15:19:51 -0600

    Trim out deps

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -3,13 +3,6 @@
             [cider/cider-nrepl "0.14.0"]
             [com.palletops/pallet-lein "0.8.0-alpha.1"]
             [lein-ancient "0.6.10"]
-            [lein-localrepo "0.5.3"]
-            [lein-marginalia "0.8.0"]
             [lein-pprint "1.1.2"]
-            [lein-tar "3.3.0"]
-            [lein-typed "0.3.5"]
             ]
-  :dependencies [[pjstadig/humane-test-output "0.8.0"]
-                 [org.clojure/tools.nrepl "0.2.12"]]
-  :injections [(require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)]}}
+  }}
