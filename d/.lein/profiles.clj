dc8f76b3aa9f6ef7f151d4f2f1a196b828d92ab2
Drew Raines <drew@raines.me>
2018-03-27 09:22:18 -0500

    Remove lein plugins

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -1,8 +1,4 @@
 {:user
  {:plugins [
-            [cider/cider-nrepl "0.15.1"]
-            [com.palletops/pallet-lein "0.8.0-alpha.1"]
-            [lein-ancient "0.6.10"]
-            [lein-pprint "1.1.2"]
             ]
   }}
