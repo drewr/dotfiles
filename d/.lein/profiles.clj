3773e8287647a2e4cf26517a8a4dac8560816aba
Drew Raines <drew@raines.me>
2018-04-13 12:41:34 -0500

    Modernize profiles.clj

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -1,4 +1,9 @@
 {:user
- {:plugins [
-            ]
-  }}
+ {:dependencies [[pjstadig/humane-test-output "0.8.3"]]
+  :injections [(require 'pjstadig.humane-test-output)
+               (pjstadig.humane-test-output/activate!)]
+  :plugins [[com.palletops/pallet-lein "0.8.0-alpha.1"]
+            [lein-ancient "0.6.10"]
+            [lein-pprint "1.1.2"]
+            [com.jakemccrary/lein-test-refresh "0.21.1"]]}}
+
