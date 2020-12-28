72f13a4b5a44b032b61390246a43090b3b1b6cf9
Drew Raines <drew@raines.me>
2020-12-27 20:31:35 -0600

    cider-nrepl 0.25.5

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -1,10 +1,10 @@
 {:user
  {:dependencies [[pjstadig/humane-test-output "0.8.3"]
                  [refactor-nrepl "2.4.0"]
-                 [cider/cider-nrepl "0.25.0-alpha1"]]
+                 [cider/cider-nrepl "0.25.5"]]
   :injections [(require 'pjstadig.humane-test-output)
                (pjstadig.humane-test-output/activate!)]
   :plugins [[lein-ancient "0.6.10"]
             [lein-pprint "1.2.0"]
             [com.jakemccrary/lein-test-refresh "0.21.1"]
-            [cider/cider-nrepl "0.25.0-alpha1"]]}}
+            [cider/cider-nrepl "0.25.5"]]}}
