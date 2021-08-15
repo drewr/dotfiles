afd424fb315f160b767b7261cab5c3cbae3be027
Drew Raines <drew@raines.me>
2021-08-15 16:07:56 -0500

    Add nix-shell for zig devel

---
+++ nix/zig.nix
@@ -0,0 +1,17 @@
+# https://github.com/ziglang/zig/wiki/development-with-nix
+
+{ pkgs ? import <nixpkgs> { } }:
+
+pkgs.mkShell {
+  hardeningDisable = [ "all" ];
+  buildInputs = with pkgs; [
+    cmake
+    gdb
+    clang_12
+    llvmPackages_12.clang-unwrapped
+    llvm_12
+    lld_12
+    ninja
+    qemu
+  ];
+}
