5629b5da51d98f2f954b282d010260022347042e
Drew Raines <drew@raines.me>
2021-09-14 13:20:50 -0500

    Add lein-count plugin

--- d/.lein/profiles.clj
+++ d/.lein/profiles.clj
@@ -6,5 +6,6 @@
                (pjstadig.humane-test-output/activate!)]
   :plugins [[lein-ancient "1.0.0-RC3"]
             [lein-pprint "1.2.0"]
+            [lein-count/lein-count "1.0.9"]
             [com.jakemccrary/lein-test-refresh "0.21.1"]
             [cider/cider-nrepl "0.26.0-SNAPSHOT"]]}}
