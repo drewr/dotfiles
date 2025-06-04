a13a65469840a81b00f37786ffcda7473abfa21f
Drew Raines <drew@raines.me>
2025-06-04 18:24:49 -0500

    Use the value of $HOME for homeDirectory

--- home/default.nix
+++ home/default.nix
@@ -1,9 +1,16 @@
 { config, pkgs, ... }:
 
+let
+  actualHome = builtins.getEnv "HOME";
+  derivedHome =
+    if pkgs.stdenv.isLinux then actualHome
+    else if pkgs.stdenv.isDarwin then actualHome
+    else "/home/${config.home.username}";
+in
 {
   programs.home-manager.enable = true;
   home.username = "aar";
-  home.homeDirectory = /Users/aar;
+  home.homeDirectory = builtins.path derivedHome;
   home.stateVersion = "22.05";
   home.file.".gitconfig".source = ./d/gitconfig;
   home.file.".somedir/xinitrc".source = ./d/xinitrc;
