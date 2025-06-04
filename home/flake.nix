3ff17d16b303872f193537479eb4db8e2c8bf17e
Drew Raines <drew@raines.me>
2025-06-04 17:27:38 -0500

    Rely on the lockfile for versions

--- home/flake.nix
+++ home/flake.nix
@@ -3,9 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs";
-    nixpkgs.rev = "c7c6a3a73ea54f8e5f7eb04618f5acea1cb34870";
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.rev = "86b95fc1ed2b9b04a451a08ccf13d78fb421859c";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
