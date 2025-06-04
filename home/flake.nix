a516ff96c69ea62d7df4dd9208ab013abe140663
Drew Raines <drew@raines.me>
2025-06-04 13:29:19 -0500

    Add outputs

--- home/flake.nix
+++ home/flake.nix
@@ -7,18 +7,18 @@
   #   home-manager.inputs.nixpkgs.follows = "nixpkgs"; # Crucial for consistent Nixpkgs version
   # };
 
-  # outputs = inputs@{ nixpkgs, home-manager, ... }: {
-  #   homeConfigurations."yourusername" = home-manager.lib.homeManagerConfiguration {
-  #     inherit (inputs) nixpkgs; # Pass nixpkgs to home.nix
-  #     # Or: pkgs = import nixpkgs { system = "x86_64-linux"; }; # If you want to specify system here
-  #     modules = [
-  #       ./home/default.nix # This is the link to your main Home Manager config
-  #     ];
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    homeConfigurations."yourusername" = home-manager.lib.homeManagerConfiguration {
+      inherit (inputs) nixpkgs; # Pass nixpkgs to home.nix
+      # Or: pkgs = import nixpkgs { system = "x86_64-linux"; }; # If you want to specify system here
+      modules = [
+        ./home/default.nix # This is the link to your main Home Manager config
+      ];
 
-  #     # You can pass extra arguments to your home.nix like this:
-  #     # extraSpecialArgs = {
-  #     #   myCustomArg = "hello";
-  #     # };
-  #   };
-  # };
+      # You can pass extra arguments to your home.nix like this:
+      # extraSpecialArgs = {
+      #   myCustomArg = "hello";
+      # };
+    };
+  };
 }
