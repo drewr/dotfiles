6cd23f535bce79d6f43954d3244e6d7014a5a640
Drew Raines <drew@raines.me>
2025-06-07 14:56:45 -0500

    Replace .config/home-manager with flake version

--- d/.config/home-manager/default.nix
+++ d/.config/home-manager/default.nix
@@ -31,11 +31,20 @@
 
     (pkgs.writeShellScriptBin "latency-tcp" ''
       #!/bin/sh
-      
+
       while true; do
         /bin/echo -n `date +%H:%M` \'\'
         tcping -c 1 dns9.quad9.net | grep -E 'time=|failed:'
         sleep 11
+      done
+    '')
+
+    (pkgs.writeShellScriptBin "my-ip" ''
+      #!/bin/sh
+
+      while true; do
+        curl -s ipinfo.io | jq -r ". | \"\(.ip) \(.city) \(.region)/\(.country) \(.org)\"" 2>/dev/null || echo waiting...
+        sleep 300;
       done
     '')
   ];
@@ -55,5 +64,3 @@
 
   programs.home-manager.enable = true;
 }
-
-
