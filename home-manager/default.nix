2d67f88b8a1554acdd81c3d9c644c1529c0c29d1
Drew Raines <drew@raines.me>
2025-06-07 14:46:42 -0500

    29D0C4E5-8918-4ED6-B615-D66AAB0757A0

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -23,7 +23,6 @@
     pkgs.xh
     pkgs.xz
 
-
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
