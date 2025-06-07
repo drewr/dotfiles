0d07839954a9888140b254f0950d8342974e8d21
Drew Raines <drew@raines.me>
2025-06-07 17:47:52 -0500

    km,cvxbkbwfjlwfjljlcxkmkm

--- d/.config/home-manager/network.nix
+++ d/.config/home-manager/network.nix
@@ -1,24 +1,8 @@
 { config, pkgs, system, una, ... }:
 
 {
-  home.packages = [
-    (pkgs.writeShellScriptBin "latency-tcp" ''
-      #!/bin/sh
-
-      while true; do
-        /bin/echo -n `date +%H:%M` \  
-        tcping -c 1 dns9.quad9.net:443 | grep -E 'time=|failed:'
-        sleep 11
-      done
-    '')
-
-    (pkgs.writeShellScriptBin "my-ip" ''
-      #!/bin/sh
-
-      while true; do
-        curl -s ipinfo.io | jq -r ". | \"\(.ip) \(.city) \(.region)/\(.country) \(.org)\"" 2>/dev/null || echo waiting...
-        sleep 300;
-      done
-    '')
-  ];
+  home.file = {
+    "bin/my-ip".source = ./d/my-ip;
+    "bin/latency-tcp".source = ./d/latency-tcp;
+  };
 }
