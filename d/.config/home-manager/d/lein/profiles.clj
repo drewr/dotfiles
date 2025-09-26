10095cb658567eb10a4cba58785808bd78ca2a45
Drew Raines <drew@raines.me>
2025-09-26 12:13:39 -0500

    Remove everything except lein plugins
    Since the other dependencies can be so project-specific, going to try
    to stop managing them centrally

--- d/.config/home-manager/d/lein/profiles.clj
+++ d/.config/home-manager/d/lein/profiles.clj
@@ -1,11 +1,7 @@
 {:user
- {:dependencies [[pjstadig/humane-test-output "0.8.3"]
-                 [refactor-nrepl "2.5.1"]
-                 [cider/cider-nrepl "0.57.0"]]
-  :injections [(require 'pjstadig.humane-test-output)
-               (pjstadig.humane-test-output/activate!)]
+ {:dependencies []
+  :injections []
   :plugins [[lein-ancient/lein-ancient "1.0.0-RC3"]
-            [lein-pprint/lein-pprint "1.2.0"]
+            [lein-pprint/lein-pprint "1.3.2"]
             [lein-count/lein-count "1.0.9"]
-            [com.jakemccrary/lein-test-refresh "0.21.1"]
-            [cider/cider-nrepl "0.57.0"]]}}
+            [com.jakemccrary/lein-test-refresh "0.26.0"]]}}
