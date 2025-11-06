1af4cc76892e02659e2df8538246c407a7755446
Drew Raines <drew@raines.me>
2025-11-06 09:52:53 -0600

    Go back to unstable

--- d/.config/home-manager/flake.nix
+++ d/.config/home-manager/flake.nix
@@ -2,9 +2,9 @@
   description = "My Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     utils.url = "github:numtide/flake-utils";
