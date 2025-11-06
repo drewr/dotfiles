687953f796238a365c22f1f4520ffd7d12544b99
Drew Raines <drew@raines.me>
2025-11-05 19:17:24 -0600

    Revert "Try to get nix from building ghc every time"
    Didn't work. This reverts commit d635abc3f98d830536f6e4ff92bd06bee85d8ac9.

--- d/.config/home-manager/flake.nix
+++ d/.config/home-manager/flake.nix
@@ -9,7 +9,6 @@
     };
     utils.url = "github:numtide/flake-utils";
     una.url = "github:jwiegley/una";
-    una.inputs.haskellNix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, home-manager, utils, una }:
