e5fbc21f622dadc057e2fcf2c422131e70f3b0d6
Drew Raines <drew@raines.me>
2026-08-14 16:33:43 -0500

    add ping1 and pingn network latency tools

--- d/.config/home-manager/network.nix
+++ d/.config/home-manager/network.nix
@@ -10,5 +10,7 @@
   home.file = {
     "bin/my-ip".source = ./d/bin/my-ip;
     "bin/latency-tcp".source = ./d/bin/latency-tcp;
+    "bin/ping1".source = ./d/bin/ping1;
+    "bin/pingn".source = ./d/bin/pingn;
   };
 }
